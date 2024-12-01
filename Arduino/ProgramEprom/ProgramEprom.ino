f43d4a860d32b48ce9245bbc25b6197e98b3df69
16cra40 <63052069+16cra40@users.noreply.github.com>
2024-12-01 17:00:37 +0000

    Updated arduino code

--- Arduino/ProgramEprom/ProgramEprom.ino
+++ Arduino/ProgramEprom/ProgramEprom.ino
@@ -1,4 +1,5 @@
 /*
+QRONK 2024
 example for changing ID.
 Modified from Waveshare examples: https://www.waveshare.com/wiki/ST3215_Servo
 */
@@ -11,23 +12,28 @@ SMS_STS sms_sts;
 #define S_RXD 18
 #define S_TXD 19
 
+//All servos are initialised with ID 1
 int ID_ChangeFrom = 1;
+//Change to a unique identifier
 int ID_Changeto   = 11;
 
 void setup()
 {
+  //Start the servo serial
   Serial1.begin(1000000, SERIAL_8N1, S_RXD, S_TXD);
   sms_sts.pSerial = &Serial1;
   delay(1000);
   sms_sts.unLockEprom(1);//unlock EPROM-SAFE
   sms_sts.writeByte(ID_ChangeFrom, SMS_STS_ID, ID_Changeto);//ID
   sms_sts.LockEprom(ID_Changeto);//EPROM-SAFE locked
+  //Let the user know what the ID has been changed to
   Serial.print("CHANGED PIN TO");
   Serial.println(ID_Changeto);
 }
 
 void loop()
 {
+  //Test the new servo ID.
    sms_sts.WritePosEx(ID_Changeto, 4000, 5000, 50);
    delay(1000);
    sms_sts.WritePosEx(ID_Changeto, 1000, 5000, 50);
--- Arduino/WritePos/WritePos.ino
+++ Arduino/WritePos/WritePos.ino
@@ -1,6 +1,7 @@
 /*
 QRONK 2024
 Example program for writing position data to 12 ST3215 Servo Motors
+Prints average the current and temperature
 Modified from Waveshare examples: https://www.waveshare.com/wiki/ST3215_Servo
 */
 
@@ -13,8 +14,11 @@ SMS_STS st;
 #define S_RXD 18
 #define S_TXD 19
 
+int cycles = 1;
+
 void setup()
 {
+  //Initialise the serial ports used for communication with the servos and computer
   Serial.begin(115200);
   Serial1.begin(1000000, SERIAL_8N1, S_RXD, S_TXD);
   st.pSerial = &Serial1;
@@ -23,37 +27,48 @@ void setup()
 
 void loop()
 {
+  //Define arrays for position data
   int sz = 12;
   u8 ids[sz];
   u16 spds[sz];
   u8 accs[sz];
-  s16 poss1[sz];
-  s16 poss2[sz];
-
-  unsigned int i = 0;
-  for (i = 0; i < sz; i++) {
+  s16 pos[sz];
+  for (int i = 0; i < sz; i++) {
     ids[i] = i+1;
-    spds[i] = (i+1)*5000/sz;
+    spds[i] = 4000;
     accs[i] = 50;
-    poss1[i] = 4095;
-    poss2[i] = 2000; 
-  }
-
-  st.SyncWritePosEx(ids, sz, poss1, spds, accs);//servo(ID1) speed=3400，acc=50，move to position=4095.
+    if (cycles %2 == 0){
+      pos[i] = 0;
+    }
+    else{
+      pos[i] = 4095;
+    }
+  } 
+  //Send position commands to the servos
+  st.SyncWritePosEx(ids, sz, pos, spds, accs);
   delay(2000);
 
-  float ave = 0;
+  //Calculate the mean temperature and current for the motors
+  float aveT = 0;
+  float aveI = 0;
   int count = 0;
   for (int i = 0; i<sz; i++){
     int temp = st.ReadTemper(i+1);
     if (temp != -1) {
-      ave += temp;
+      aveT += temp;
+      aveI += st.ReadCurrent(i+1);
       count ++;
     }
   }
-  ave /=count;
-  Serial.println(ave);
+  aveT /= count;
+  aveI /= count;
 
-  st.SyncWritePosEx(ids, sz, poss2, spds, accs);//servo(ID1) speed=3400，acc=50，move to position=4095.
-  delay(2000);
+  //Print the temps and currents to serial
+  Serial.print("Temperature(C):");
+  Serial.print(aveT);
+  Serial.print(",");
+  Serial.print("Current(A):");
+  Serial.println(aveI);
+
+  cycles ++;
 }
